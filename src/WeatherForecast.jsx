feb4e7cbfa675aff8a5747aad95f52cc7aec11a0
Tamerlan Mustafayev <tamerlan.m.2000@gmail.com>
2024-10-23 20:29:00 -0400

    create weatherForecast component

---
+++ src/WeatherForecast.jsx
@@ -0,0 +1,14 @@
+import './WeatherForecast.css'
+
+
+const WeatherForecast = () => {
+    <div className="weather">
+    <h2>Day of the Week</h2>
+    <img src="" alt="" />
+    <p><span>conditions: </span>current weather conditions</p>
+    <p><span>time: </span>time of day</p>
+</div> 
+}
+
+export default WeatherForecast
+
